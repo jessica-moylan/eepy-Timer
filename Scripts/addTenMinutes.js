17dcf7413195d7e6d63a03399d52edd2630e62ca
shamah66 <73977059+shamah66@users.noreply.github.com>
2024-02-04 11:12:54 -0500

    added second button

--- Scripts/addTenMinutes.js
+++ Scripts/addTenMinutes.js
@@ -8,4 +8,15 @@ function addTenMinutes() {
     // alert("Added 10 minutes to sleep time");
 }
 
+function removeFifteenMinutes() {
+    chrome.storage.local.get().then((result) => {
+        let currentExtensionSeconds = result["sleepTimeExtensionSeconds"];
+        let newExtensionSeconds = currentExtensionSeconds - 900;
+        chrome.storage.local.set({"sleepTimeExtensionSeconds": newExtensionSeconds});
+    })
+    console.log("removed 15 minutes from sleep time");
+    // alert("Added 10 minutes to sleep time");
+}
+
 document.getElementById("delayButton").addEventListener("click", addTenMinutes);
+document.getElementById("speedButton").addEventListener("click", removeFifteenMinutes);
